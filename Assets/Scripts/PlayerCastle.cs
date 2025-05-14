035432794616b9597e179f03c1535f03e60fb021
MinhNHN <minhnhn897@gmail.com>
2025-05-14 14:53:01 +0700

    Add Player Castle as final destination for enemies
    Added a final destination for enemies: the Player Castle. When an enemy's collider touches the castle's collider, a function is triggered that destroys the enemy object.

---
+++ Assets/Scripts/PlayerCastle.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCastle : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
