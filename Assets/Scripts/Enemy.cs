da13b661fc41b741d80ec53c638cc4fd3a9a66bd
MinhNHN <minhnhn897@gmail.com>
2025-05-14 10:09:35 +0700

    Created a function that control the Enemy's ratation when it's moving from waypoint to waypoint

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoint;
+    [SerializeField] private float turnSpeed = 10f;
+
+    private NavMeshAgent agent;
+    private int waypointIndex;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     agent.SetDestination(waypoint.position);
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FaceTarget(agent.steeringTarget);
+        
+        // Check if the agent is close to current target point
+        // Then set a destination for the next waypoint
+        if (agent.remainingDistance < 0.5f)
+        {
+            agent.SetDestination(GetNextWayPoint());
+        }
+    }
+
+    private Vector3 GetNextWayPoint()
+    {
+        if (waypointIndex >= waypoint.Length)
+        {
+            return transform.position;
+        }
+
+        Vector3 targetPosition = waypoint[waypointIndex].position;
+        waypointIndex++;
+        return targetPosition;
+    }
+
+    private void FaceTarget(Vector3 newTarget)
+    {
+        // Calculate the direction from current position to next target
+        // Draw a vector from point A to point B essentially
+        Vector3 directionToTarget = newTarget - transform.position;
+
+        // Ignore any difference in vertical position
+        directionToTarget.y = 0; 
+
+        // Create a new rotation that points the forward vector (y) of the game object up to the calculated direction
+        // Since we ignore the vertical position above, the game object can only rotate horizontally
+        Quaternion newRotation = Quaternion.LookRotation(directionToTarget);
+
+        // Create a smooth rotation from the current rotation to the target rotation at a defined speed
+        // Time.deltaTime makes this framerate independent
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime);
+    }
+}
