0b1419ae9b1576c21fe187cb77ae7e8bc9219365
MinhNHN <minhnhn897@gmail.com>
2025-05-20 11:03:54 +0700

    Add emission intensity coroutine to crossbow tower
    Implemented a coroutine that gradually increases the emission intensity
    of the crossbow material from 0 to a maximum value over a fixed duration.

--- Assets/Scripts/CrossbowVisual.cs
+++ Assets/Scripts/CrossbowVisual.cs
@@ -6,11 +6,22 @@ public class CrossbowVisual : MonoBehaviour
     [SerializeField] private LineRenderer attackLine;
     [SerializeField] private float attackVisualDur = .1f;
 
+    [Header("Glowing Visual")]
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Material materialInstance;
+    [SerializeField] private float currentIntensity;
+    [SerializeField] private float maxIntensity = 150f;
+
     private TowerCrossbow mainTower;
 
     void Awake()
     {
         mainTower = GetComponent<TowerCrossbow>();
+
+        materialInstance = new Material(meshRenderer.material);
+        meshRenderer.material = materialInstance;
+
+        StartCoroutine(ChangeEmission(3));
     }
 
     public void PlayAttackFX(Vector3 startPoint, Vector3 endPoint)
@@ -30,5 +41,24 @@ public class CrossbowVisual : MonoBehaviour
 
         mainTower.EnableRotation(true);
         attackLine.enabled = false;
+    }
+
+    private IEnumerator ChangeEmission(float duration)
+    {
+        // Record the moment when this coroutine start
+        float startTime = Time.time;
+        float startIntensity = 0f;
+
+        // Repeat every frame until the full duration has passed
+        while (Time.time - startTime < duration)
+        {
+            // Calculate the proportion of the duration has elapsed since this coroutine start
+            float tValue = (Time.time - startTime) / duration;
+            // Smoothly increase the intensity based on current progress (tValue)
+            currentIntensity = Mathf.Lerp(startIntensity, maxIntensity, tValue);
+            yield return null; // wait for the next frame
+        }
+
+        currentIntensity = maxIntensity;
     }
 }
