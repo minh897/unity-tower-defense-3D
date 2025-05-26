c912c3a15056ca8a33a308d2e8ddd35b7ed4dde0
MinhNHN <minhnhn897@gmail.com>
2025-05-26 14:14:32 +0700

    Add a way to build nav mesh when switch to road tile
    When switch to tile with layer road, the Editor can now bake (or build) a new nav mesh

--- Assets/Scripts/TileSystem/TileSlot.cs
+++ Assets/Scripts/TileSystem/TileSlot.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class TileSlot : MonoBehaviour
     private MeshRenderer tileMeshRenderer => GetComponent<MeshRenderer>();
     private MeshFilter tileMeshFilter => GetComponent<MeshFilter>();
     private Collider tileCollider => GetComponent<Collider>();
+    private NavMeshSurface tileNavMesh => GetComponentInParent<NavMeshSurface>();
 
 
     public Material GetMaterial() => tileMeshRenderer.sharedMaterial;
@@ -16,34 +18,31 @@ public class TileSlot : MonoBehaviour
     public Collider GetCollider() => tileCollider;
 
 
-    public void AdjustYRotation(int dir) => transform.Rotate(0, 90 * dir, 0);
+    public void AdjustYRotation(int dir)
+    {
+        transform.Rotate(0, 90 * dir, 0);
+        UpdateNavMesh();
+    }
 
-    public void AdjustYPosition(int verticalDir) => transform.position += new Vector3(0, 0.1f * verticalDir, 0);
+    public void AdjustYPosition(int verticalDir)
+    {
+        transform.position += new Vector3(0, 0.1f * verticalDir, 0);
+        UpdateNavMesh();
+    }
 
-    
     public void SwitchTile(GameObject referenceTile)
     {
         gameObject.name = referenceTile.name;
-        
+
         TileSlot newTile = referenceTile.GetComponent<TileSlot>();
 
         tileMeshRenderer.material = newTile.GetMaterial();
         tileMeshFilter.mesh = newTile.GetMesh();
 
         UpdateCollider(newTile.GetCollider());
-
-        foreach (GameObject child in GetAllChildren())
-        {
-            // Use this because Detroy doesn't work in Editor
-            DestroyImmediate(child);
-        }
-
-        // Find every child game object in the new tile
-        // and create them in the selected tile
-        foreach (GameObject obj in newTile.GetAllChildren())
-        {
-            Instantiate(obj, transform);
-        }
+        UpdateChildrenObject(newTile);
+        UpdateLayer(referenceTile);
+        UpdateNavMesh();
     }
 
     public List<GameObject> GetAllChildren()
@@ -82,4 +81,23 @@ public class TileSlot : MonoBehaviour
         }
     }
 
+    private void UpdateChildrenObject(TileSlot newTile)
+    {
+        foreach (GameObject child in GetAllChildren())
+        {
+            // Use this because Detroy doesn't work in Editor
+            DestroyImmediate(child);
+        }
+
+        // Find every child game object in the new tile
+        // and create them in the selected tile
+        foreach (GameObject obj in newTile.GetAllChildren())
+        {
+            Instantiate(obj, transform);
+        }
+    }
+
+    private void UpdateLayer(GameObject referenceObj) => gameObject.layer = referenceObj.layer;
+
+    private void UpdateNavMesh() => tileNavMesh.BuildNavMesh();
 }
