29adf810c8295ecba17c33740b6c339a3ee1583d
MinhNHN <minhnhn897@gmail.com>
2025-05-28 10:33:35 +0700

    Add camera handle rotation method
    Camera can now be rotated up and down by holding the right mouse button in Play mode. The camera's rotations are clamped within safe ranges. Avoid cases where Player might rotate under the level or flip the camera by accident

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,12 +4,24 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 100f;
 
+    [Header("Rotation Settings")]
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float maxFocusPointDistance = 15f;
+    [SerializeField] private Transform focusPoint;
+    [Space]
+    [SerializeField] private float minPitch = 5f;
+    [SerializeField] private float maxPitch = 85f;
+    private float pitch;
+
     private float smoothTime = .1f;
     private Vector3 movementVelocity = Vector3.zero;
 
     void Update()
     {
         HandleMovement();
+        HandleRotation();
+
+        focusPoint.position = transform.position + transform.forward * GetFocusPointDistance();
     }
 
     public void HandleMovement()
@@ -29,8 +41,39 @@ public class CameraController : MonoBehaviour
             targetPosition += transform.right * moveSpeed * Time.deltaTime;
         if (hInput < 0)
             targetPosition -= transform.right * moveSpeed * Time.deltaTime;
-        
+
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref movementVelocity, smoothTime);
+    }
+
+    public void HandleRotation()
+    {
+        // Check if the right mouse button is being hold
+        if (Input.GetMouseButton(1))
+        {
+            float hRotation = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+            float vRotation = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+
+            // Adjusts the vertical angle and Clamp it to keep it in safe ranges
+            pitch = Mathf.Clamp(pitch - vRotation, minPitch, maxPitch);
+
+            // Rotate horizontally around the focus point using the world's up axis
+            transform.RotateAround(focusPoint.position, Vector3.up, hRotation);
+
+            // Rotate vertically around the point using local right axis
+            // Smoothly adjust the angle by the difference between desired pitch and current x rotation
+            transform.RotateAround(focusPoint.position, transform.right, pitch - transform.eulerAngles.x);
+
+            // Make the camera always look at the focus point
+            transform.LookAt(focusPoint);
+        }
+    }
+
+    private float GetFocusPointDistance()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxFocusPointDistance))
+            return hit.distance;
+
+        return maxFocusPointDistance;
     }
 }
