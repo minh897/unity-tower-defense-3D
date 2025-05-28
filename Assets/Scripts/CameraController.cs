7839cc008a8aed178d3e72254cb114c58c56d0af
MinhNHN <minhnhn897@gmail.com>
2025-05-28 14:36:13 +0700

    Add panning to mouse movement
    You can now pan the camera around when holding middle mouse button and drag it in the opposite of the direction you want to go.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,31 +2,37 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 100f;
 
-    [Header("Rotation Settings")]
+    [Header("Rotation")]
     [SerializeField] private float rotateSpeed;
-    [SerializeField] private float maxFocusPointDistance = 15f;
+    [SerializeField] private float focusPointDistance = 15f;
     [SerializeField] private Transform focusPoint;
     [Space]
     [SerializeField] private float minPitch = 5f;
     [SerializeField] private float maxPitch = 85f;
     private float pitch;
 
-    [Header("Zoom Settings")]
+    [Header("Zooming")]
     [SerializeField] private float zoomSpeed = 10f;
     [SerializeField] private float minZoom = 3f;
     [SerializeField] private float maxZoom = 15f;
 
+    [Header("Mouse Movement")]
+    [SerializeField] private float moveSpeed = 120f;
+    [SerializeField] private float mouseSpeed = 5f;
+
     private float smoothTime = .1f;
     private Vector3 movementVelocity = Vector3.zero;
     private Vector3 zoomVelocity = Vector3.zero;
+    private Vector3 mouseMovementVelocity = Vector3.zero;
+    private Vector3 lastMousePosition;
 
     void Update()
     {
         HandleMovement();
         HandleRotation();
         HandleZoom();
+        HandleMouseMovement();
 
         focusPoint.position = transform.position + transform.forward * GetFocusPointDistance();
     }
@@ -96,11 +102,41 @@ public class CameraController : MonoBehaviour
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref zoomVelocity, smoothTime);
     }
 
+    private void HandleMouseMovement()
+    {
+        // Store the current mouse position when press middle mouse button
+        if (Input.GetMouseButtonDown(2))
+            lastMousePosition = Input.mousePosition;
+
+        if (Input.GetMouseButton(2))
+        {
+            // Calculate the change in position between the current mouse input and the last
+            Vector3 positionDifference = Input.mousePosition - lastMousePosition;
+
+            // Convert mouse movement into world space movement
+            Vector3 moveRight = transform.right * (-positionDifference.x) * mouseSpeed * Time.deltaTime;
+            Vector3 moveForward = transform.forward * (-positionDifference.y) * mouseSpeed * Time.deltaTime;
+
+            // Prevent vertical movement
+            moveRight.y = 0;
+            moveForward.y = 0;
+
+            // Combine right and forward movement into a single movement vector
+            Vector3 movement = moveRight + moveForward;
+            Vector3 targetPosition = transform.position + movement;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref mouseMovementVelocity, smoothTime);
+
+            // Save the current mouse position to compare it in the next frame
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+
     private float GetFocusPointDistance()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxFocusPointDistance))
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, focusPointDistance))
             return hit.distance;
 
-        return maxFocusPointDistance;
+        return focusPointDistance;
     }
 }
