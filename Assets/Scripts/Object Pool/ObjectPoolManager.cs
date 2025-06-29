13c43558a9ca63048c60a11b2cd684f41a6cbcf4
MinhNHN <minhnhn897@gmail.com>
2025-06-29 15:19:07 +0700

    Add object pooling system with preload and reuse support
    - Implement ObjectPoolManager as a singleton to manager pooled objects by prefab
    - Automatically initilize predefined pools at startup with configurable default and max sizes.
    - Add support for lazy pool creation when a requested prefab has no predefined pool.
    - Preload objects asynchronously using a coroutine to reduce runtime instatiation cost.
    - Use PooledObject component to track and safely return objects to their original pool.

---
+++ Assets/Scripts/Object Pool/ObjectPoolManager.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    public static ObjectPoolManager instance;
+
+    [Header("Object Pool Details")]
+    [SerializeField] private GameObject[] predefinedPool;
+    [SerializeField] private int defaultPoolSize = 50;
+    [SerializeField] private int maxPoolSize = 500;
+
+    private Dictionary<GameObject, ObjectPool<GameObject>> poolDictionary;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Start()
+    {
+        InitializePools();
+    }
+
+    public GameObject Get(GameObject prefab, Vector3 position, Quaternion? rotation = null, Transform parent = null)
+    {
+        if (poolDictionary.ContainsKey(prefab) == false)
+        {
+            Debug.LogWarning("No pool was founded for " + gameObject.name + ". Creating new pool");
+            CreateNewPool(prefab);
+        }
+
+        GameObject objectToGet = poolDictionary[prefab].Get();
+        objectToGet.transform.position = position;
+        objectToGet.transform.rotation = rotation ?? Quaternion.identity;
+        objectToGet.transform.parent = parent;
+
+        return objectToGet;
+    }
+
+    public void Remove(GameObject objectToRemove)
+    {
+        GameObject originalPrefab = objectToRemove.GetComponent<PooledObject>()?.originalPrefab;
+
+        if (poolDictionary.ContainsKey(originalPrefab) == false || originalPrefab == null)
+        {
+            Debug.LogWarning("This game object is destroyed because it doesn't have ObjectPool component.");
+            Destroy(objectToRemove);
+            return;
+        }
+
+        poolDictionary[originalPrefab].Release(objectToRemove);
+    }
+
+    private void InitializePools()
+    {
+        poolDictionary = new();
+
+        foreach (GameObject prefab in predefinedPool)
+        {
+            CreateNewPool(prefab);
+        }
+    }
+
+    private void CreateNewPool(GameObject prefab)
+    {
+        var pool = new ObjectPool<GameObject>
+            (
+                createFunc: () => NewPoolObject(prefab),
+                actionOnGet: obj => obj.SetActive(true),
+                actionOnRelease: obj =>
+                {
+                    obj.SetActive(false);
+                    obj.transform.parent = transform;
+                },
+                actionOnDestroy: obj => Destroy(obj),
+                collectionCheck: false,
+                defaultCapacity: defaultPoolSize,
+                maxSize: maxPoolSize
+            );
+
+        poolDictionary.Add(prefab, pool);
+        StartCoroutine(PreloadPoolCo(pool, defaultPoolSize));
+    }
+
+    private IEnumerator PreloadPoolCo(ObjectPool<GameObject> poolToPreload, int count)
+    {
+        List<GameObject> preloadObjects = new();
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj = poolToPreload.Get();
+            preloadObjects.Add(obj);
+            obj.SetActive(false);
+            yield return null;
+        }
+
+        foreach (GameObject obj in preloadObjects)
+        {
+            poolToPreload.Release(obj);
+        }
+    }
+
+    private GameObject NewPoolObject(GameObject prefab)
+    {
+        GameObject newObject = Instantiate(prefab);
+        newObject.AddComponent<PooledObject>().originalPrefab = prefab;
+
+        return newObject;
+    }
+}
